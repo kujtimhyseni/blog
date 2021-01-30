6e18ff67519155aeb5548df1c79364c7f6833feb
Kushtrim Pacaj <kushtrimpacaj@gmail.com>
2021-01-30 15:26:23 +0100

    Added communication with sqlite db and a sample rest api that returns contents of a test table

--- blog_backend/src/app.js
+++ blog_backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require('http');
 
 const hostname = '127.0.0.1'
+const DB_FILE_PATH = "./blog.db"
 const port = 8080
 
 const app = express();
@@ -9,7 +10,23 @@ const app = express();
 http.createServer(app).listen(port);
 console.log(`Server running at http://${hostname}:${port}/`)
 
-app.get("/", function (req, res) {
-    res.send("Hello world");
-});
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
 
+let blogDb;
+
+(async () => {
+    blogDb = await open({filename: DB_FILE_PATH, driver: sqlite3.Database})
+})()
+
+app.get("/", async function (req, res) {
+    try {
+        const query = 'SELECT name, age FROM test;'
+        const row = await blogDb.get(query);
+        console.log(row)
+        res.json(row)
+    } catch (e) {
+        res.json({error: e.message})
+    }
+
+});
