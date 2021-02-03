d8c30fe2608a4465018777bd4da4013f95f12672
Kushtrim Pacaj <kushtrimpacaj@gmail.com>
2021-02-04 00:28:24 +0100

    Use cards on comments. Better UI

--- blog_frontend/src/components/view-comment-compoent.js
+++ blog_frontend/src/components/view-comment-compoent.js
@@ -1,19 +1,43 @@
 import React, {Component} from "react";
-import {withStyles} from "@material-ui/core";
+import {Card, CardContent, makeStyles, withStyles} from "@material-ui/core";
 import {styles} from "../css-common";
+
+
+const style = {
+    root: {
+        marginTop:"15px",
+        marginBottom:"15px",
+        minWidth: 275,
+        backgroundColor: "#F0F0F0"
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
 
 
 class ShowComment extends Component {
     render() {
         const {comment, classes} = this.props
         return (
-            <div className={classes.BlogComment}>
-                <h4>Comment author: {comment.author_name} <br/> Comment email: {comment.author_email}</h4>
-                <span>{comment.content}</span>
-
-            </div>
+            <Card className={classes.root}>
+                <CardContent>
+                    <div >
+                        <h4>Comment author: {comment.author_name} <br/> Comment email: {comment.author_email}</h4>
+                        <span>{comment.content}</span>
+                    </div>
+                </CardContent>
+            </Card>
         )
     }
 }
 
-export default withStyles(styles)(ShowComment)
+export default withStyles(style)(ShowComment)
