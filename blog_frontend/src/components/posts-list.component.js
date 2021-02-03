483e4f3dbd4c4cb7c1640490cda84be59f19e56f
Kushtrim Pacaj <kushtrimpacaj@gmail.com>
2021-02-03 22:22:29 +0100

    Process ENTER key on search tag input the same as if search button was called

--- blog_frontend/src/components/posts-list.component.js
+++ blog_frontend/src/components/posts-list.component.js
@@ -60,6 +60,12 @@ class PostsList extends Component {
         });
     }
 
+    handleOnKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            this.retrievePosts()
+        }
+    }
+
     retrievePosts = () => {
         PostDataService.getAllBlogPosts(this.state.searchTag)
             .then(response => {
@@ -139,6 +145,7 @@ class PostsList extends Component {
                 <TextField
                     label="Search by tag"
                     value={searchTag}
+                    onKeyDown={this.handleOnKeyDown}
                     onChange={this.onChangeSearchTag}
                 />
                 <Button
